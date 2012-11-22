4aa7d529ace9281db49aeb0be4dbf3f0ff5abc69
Ryan Knuesel <ryan@ryanknu.com>
2012-11-21 18:03:34 -0600

    Removing translator from skeleton

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -18,7 +18,6 @@ class Module
 {
     public function onBootstrap(MvcEvent $e)
     {
-        $e->getApplication()->getServiceManager()->get('translator');
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -107,18 +107,7 @@ return array(
     ),
     'service_manager' => array(
         'factories' => array(
-            'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
             'Smarty' => 'ZSmarty\StrategyFactory',
-        ),
-    ),
-    'translator' => array(
-        'locale' => 'en_US',
-        'translation_file_patterns' => array(
-            array(
-                'type'     => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
-            ),
         ),
     ),
     'controllers' => array(
